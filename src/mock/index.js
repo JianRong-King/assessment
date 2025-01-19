41acbe1a79e76771e21604d28992968c8809f28f
King Jian Rong <kingjianrong3249@gmail.com>
2025-01-19 14:55:43 +0800

    Handled the backend mock database for category select

--- src/mock/index.js
+++ src/mock/index.js
@@ -7,7 +7,19 @@ createServer({
     this.namespace = 'api';
 
     this.get('/posts', () => {
-      return data;
+      return data.posts;
+    });
+
+    this.get('/posts/category', (schema, request) => {
+      const { categories } = request.queryParams;
+      const categoryList = categories ? categories.split(',') : []; // Split comma-separated values
+
+      // Use filter to get posts matching the selected categories
+      const filteredPosts = data.posts.filter((post) =>
+        post.categories.some((category) => categoryList.includes(category.name))
+      );
+
+      return filteredPosts;
     });
   },
 });
