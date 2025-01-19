f8f6b10348ef6cce6cb7e93018cac0d833769904
King Jian Rong <kingjianrong3249@gmail.com>
2025-01-19 14:52:38 +0800

    Achieved multi-select category filter
    - By getting the unique categories
    - Handle the filter in our mock backend database

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,29 +1,81 @@
 import React, { useEffect, useState } from 'react';
+import CategoryFilter from './util/CategoryFilter';
+import Button from './util/Button';
 
 export default function Content() {
-  const [data, setData] = useState([]);
-
+  const [orignalData, setOrignalData] = useState([]);
   const itemsPerPage = 5;
   const [currentPage, setCurrentPage] = useState(1);
-  const maxPages = Math.ceil(data.length / itemsPerPage);
+  const [maxPage, setMaxPage] = useState(0);
+  const [uniqueCategory, setUniqueCategory] = useState([]);
+  const [currentData, setCurrentData] = useState([]);
 
-  const currentItems = data.slice(
+  const currentItems = currentData.slice(
+    // mutating array items
     (currentPage - 1) * itemsPerPage,
     currentPage * itemsPerPage
   );
 
   useEffect(() => {
+    // called only once
     const fetchData = async () => {
       const response = await fetch('/api/posts');
       const responseJson = await response.json();
-
-      return responseJson.posts;
+      return responseJson;
     };
-    fetchData().then((data) => setData(data));
+    fetchData().then((data) => {
+      setCurrentData(data);
+      setOrignalData(data);
+      setMaxPage(Math.ceil(data.length / itemsPerPage));
+      const categories = new Map();
+      for (const dataObj of data) {
+        for (const category of dataObj.categories) {
+          if (!categories.has(category.name)) {
+            categories.set(category.name, {
+              name: category.name,
+              selected: false,
+            });
+          }
+        }
+      }
+      setUniqueCategory([...categories.values()]);
+    });
   }, []);
+
+  useEffect(() => {
+    // get the selected categoreis data
+    const selectedCategory = [];
+    uniqueCategory.forEach((cateobj) =>
+      cateobj.selected ? selectedCategory.push(cateobj.name) : ''
+    );
+
+    if (selectedCategory.length) {
+      const reqParam = `?categories=${selectedCategory.join(',')}`; // Use comma-separated categories
+
+      const fetchSelectedData = async () => {
+        const response = await fetch(`/api/posts/category${reqParam}`); // Use '/category' route
+        const responseJson = await response.json();
+        return responseJson;
+      };
+
+      fetchSelectedData().then((data) => {
+        console.log(data);
+        setCurrentData(data);
+        setMaxPage(Math.ceil(data.length / itemsPerPage));
+      });
+    } else {
+      setCurrentData(orignalData);
+      setMaxPage(Math.ceil(orignalData.length / itemsPerPage));
+    }
+  }, [uniqueCategory, orignalData]);
 
   return (
     <div>
+      <CategoryFilter
+        uniqueCategory={uniqueCategory}
+        setUniqueCategory={setUniqueCategory}
+      />
+
       {currentItems.map((item) => (
         <ul key={item.id}>
           <img alt="Author" src={item.author.avatar} />
@@ -34,22 +86,11 @@ export default function Content() {
         </ul>
       ))}
 
-      <button
-        onClick={() => {
-          setCurrentPage((prev) => prev - 1);
-        }}
-        disabled={currentPage === 1}
-      >
-        Previous Page
-      </button>
-      <button
-        onClick={() => {
-          setCurrentPage((prev) => prev + 1);
-        }}
-        disabled={currentPage === maxPages}
-      >
-        Next Page
-      </button>
+      <Button
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+        maxPages={maxPage}
+      />
     </div>
   );
 }
